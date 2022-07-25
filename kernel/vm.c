e31bfaa6bff3f3f5b2ba49d5d82a177170ff5573
jingfenhuanzhe1 <ab203230@gmail.com>
2022-07-25 01:11:39 -0700

    modify

--- kernel/vm.c
+++ kernel/vm.c
@@ -38,7 +38,7 @@ void kvm_map_pagetable(pagetable_t pgtbl) {
   kvmmap(pgtbl, KERNBASE, KERNBASE, (uint64)etext-KERNBASE, PTE_R | PTE_X);
 
   // map kernel data and the physical RAM we'll make use of.
-  kvmmap(pgtbl, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
+  kvmmap(pgtbl, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);   //kernel的data部分
 
   // map the trampoline for trap entry/exit to
   // the highest virtual address in the kernel.
